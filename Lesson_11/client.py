40c689ff8174ed11665ab3be139bd89f3575a6dd
OleksiySorochan <oleksiy.sorochan@gmail.com>
2023-06-01 20:31:21 +0300

    les_11 update client.py master

--- Lesson_11/client.py
+++ Lesson_11/client.py
@@ -1,7 +1,7 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('localhost', 55000))
+sock.connect(('localhost', 55000))
 sock.send(bytes("This is client, answer me", encoding='UTF-8'))
 data = sock.recv(1024)
 sock.close()
